4906e38bf2705cd569bf9067d24fcd2d29971517
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-10 17:19:10 -0500

    add console log for database connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("./models/workout");
 const express = require("express");
 const mongoose = require("mongoose");
-// const morgan = require("morgan");
+// const logger = require("morgan");
 const htmlRoutes = require("./routes/html-routes");
 const apiRoutes = require("./routes/api-routes");
 
@@ -12,12 +12,18 @@ mongoose.connect("mongodb://localhost:27017/workout", {
   useCreateIndex: false,
 });
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
-// app.use(morgan("combined"));
+// app.use(morgan("dev"));
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
