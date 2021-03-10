cfaa1d65ca8be20b1c1f05d36f0dbd91be140f7c
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-10 11:44:50 -0500

    create and require the routes

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "Connection Error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
+const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
