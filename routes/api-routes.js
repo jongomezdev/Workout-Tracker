5e773c2acf649aa879c5b0099b6ac5087a4beaa4
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-10 16:21:12 -0500

    create router for workouts in range

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,7 +19,27 @@ router.get("/api/workouts", async (req, res, next) => {
           day: 1,
         },
       },
-    ]);
+    ]).exec();
+    res.json(workouts);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/api/workouts/range", async (req, res, next) => {
+  try {
+    const workouts = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+      { $sort: { day: -1 } },
+      { $limit: 7 },
+      { $sort: { day: 1 } },
+    ]).exec();
   } catch (e) {
     next(e);
   }
