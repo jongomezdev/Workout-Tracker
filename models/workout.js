2d4093bce649a6e4b25f89213379d7057dd497f9
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-10 13:19:07 -0500

    create route for /exercise

--- models/workout.js
+++ models/workout.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Exercise = require("./exercise");
 
 const workoutSchema = new Schema({
-  exercises: foo,
-  day: {
-    type: Date,
-    default: Date.now,
-  },
+  exercises: [Exercise],
+  day: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("workout", workoutSchema);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,4 +6,8 @@ router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
 module.exports = router;
