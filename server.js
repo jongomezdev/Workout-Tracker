0a63e5609d51ea752d6050fe167f87d5fd4a3e56
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-10 12:06:31 -0500

    connect routes to get ride of middleware function error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const htmlRoutes = require("./routes/html-routes");
 const apiRoutes = require("./routes/api-routes");
 
@@ -6,9 +7,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("combined"));
+
 app.use(htmlRoutes);
 app.use(apiRoutes);
 
 app.listen(3000, () => {
   console.log("Serving on port 3000");
 });
+
+module.exports = app;
